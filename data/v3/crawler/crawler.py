87df60bf5c5588524b8e54240f1c8f18990715d3
Dimitri <dimitri.guitton.dev@gmail.com>
2024-05-06 10:33:40 +0200

    feat(scraping): recupération des images avec leur id

--- data/v3/crawler/crawler.py
+++ data/v3/crawler/crawler.py
@@ -6,13 +6,14 @@ import pandas as pd
 import io
 # Use Pillow to convert the Python object to an RGB image
 from PIL import Image
-# hashlib allows you to get hashes. Let's use 'sha1' to name the images.
-import hashlib
 # pathlib lets you point to specific directories. Use it to store the images in a folder.
 from pathlib import Path
+import os
+import progressbar
 
 def get_items_data():
     # Scraping items data
+    print("Scraping items data")
     url = "https://www.techtonica-calculator.com/seiten/Items.php"
     script_index = 5
 
@@ -26,7 +27,7 @@ def get_items_data():
     return data
 
 def get_items_image():
-    # Scraping items image
+    print("Scraping items images")
     url = "https://www.techtonica-calculator.com/seiten/Items.php"
 
     req = requests.get(url)
@@ -34,30 +35,30 @@ def get_items_image():
     results = []
 
     base_url = 'https://www.techtonica-calculator.com/'
-    for a in soup.findAll('img'):
+    for div in soup.findAll('div', class_='p2px m2l zentrieren rad10 backcolor_tiefdunkelblau color_orange border_blau s05'):
+        href = div.find('a').get('href')
+        item_id = href.split('=')[1]
+        img_src = div.find('img').get('src')
         # Si la source commence par "../bilder/"
-        src = a.get("src")
-        if src is not None and src.startswith("../bilder/") and src not in results:
+        if img_src is not None and img_src.startswith("../bilder/") and img_src not in results:
             # On ajoute l'URL de base et on ajoute la source sans les "../bilder"
-            full_url = base_url + src[3:]
+            full_url = base_url + img_src[3:]+f'?id={item_id}'
             results.append(full_url)
 
     # Permet de checker les résultats
     df = pd.DataFrame({"links": results})
     df.to_csv("../raw_data/img_links.csv", index = False, encoding = "utf-8")
 
-    print(results)
-    for b in results:
+    for b in progressbar.progressbar(results):
         # Store the content from the URL to a variable
         image_content = requests.get(b).content
         # Create a byte object out of image_content and store it in the variable image_file
         image_file = io.BytesIO(image_content)
         # Use Pillow to convert the Python object to an RGB image
         image = Image.open(image_file).convert("RGB")
-        # Set a file_path variable that points to your directory.
-        # Create a file based on the sha1 hash of 'image_content'.
-        # Use .hexdigest to convert it into a string.
-        file_path = Path("../dist/images", hashlib.sha1(image_content).hexdigest()[:10] + ".png")
+
+        item_id = b.split('=')[1]
+        file_path = Path("../dist/images", os.path.basename(item_id + ".png"))
         image.save(file_path, "PNG", quality = 80)
 
     return
@@ -75,4 +76,4 @@ def get_raw_items():
     return data
 
 
-get_items_image()
+run_scraping()
